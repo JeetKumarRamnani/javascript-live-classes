338cc0154b0eadeeb90aa782218febd5bab02266
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-01-10 20:57:45 +0500

    javascript live classes

---
+++ Class 1/index.js
@@ -0,0 +1,12 @@
+//Variables
+// ECMASCRIPT 6 IN 2015 IS ALSO KNOWN AS ES6 THIS IS VERY IMPORTANT
+//IF YOU WANT TO LEARN REACT YOU HAVE TO MASTER ES6
+
+//variables before es6
+
+// var container = "This Is A String";
+// container = "This is another string";
+
+// console.log(container);
+ 
+// var a:string = "hello"
---
+++ Class 2/index.js
@@ -0,0 +1,61 @@
+// let obj = {
+//   name: "Jeet Kumar",
+//   age: 27,
+// };
+
+// Object Literal
+
+// obj.adress = "Islamabad";
+// console.log(obj);
+// console.log(obj.age);
+// console.log(obj.name);
+// console.log(obj["age"]);
+
+// let name = "Jeet Kumar"; //string literal
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr[3]);
+
+// const obj2 = new Object(); //Object Constructor KE Through Ye JAVASCRIPT Hamein Provide Karta Hai
+// obj2.name = "Karan Kumar";
+// obj2.age = 25;
+// console.log(obj2);
+
+// const obj3 = {};
+// obj3.name = "Ali";
+// console.log(obj3);
+
+// Simple Function Without Perameters
+// function Add() {
+//   console.log("Jeet Kumar");
+// }
+
+// Add();
+// Add();
+
+// Functions With Parametrs
+
+// var a; //By Default Agar Aap Value Na Do Toh Javascript Usko Undefined Assign Kardeta Hai
+
+// console.log(a + 2);//NaN =Not A Number
+
+// function Add(a, b) {
+//   console.log(a + b);
+// }
+
+// Add(2,3);
+// let naam = "jeet";
+// const object = {
+//   naam: "Jeet",
+//   age: 27,
+
+//   printName: function () {
+//     console.log(naam);
+//   }, //this is called method its a function but in terms of object its called method
+// };
+
+// object.printName();
+
+// console.log(this);
+
+// window.alert("Hello"); //window is global object
