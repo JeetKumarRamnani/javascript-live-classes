672f09834301bd306c4ab338b5260376c6fd336b
Jeet Kumar Ramnani <jeetkramnani@gmail.com>
2023-01-11 20:40:47 +0500

    Class 3

---
+++ Class 3/index.js
@@ -0,0 +1,48 @@
+//Normal Function Without Return
+// function Add(a, b) {
+//   console.log(a + b);
+// }
+
+// Add(2, 3);
+
+// function sub(a, b) {
+//   console.log(a - b);
+// }
+
+// sub(2, 3);
+
+//Return Keyword In Javascript
+
+// function Addition(a,b) {
+//   return a+b;
+// }
+
+// let Result = Addition(2,2);
+
+// console.log(Result);
+
+//What If I Use any Statement Below Return Keyword
+
+// function Multiplication(a, b) {
+
+//   return a * b;
+//   console.log("Hello World");
+// }
+
+// let Result = Multiplication(2, 2);
+
+// console.log(Result);
+
+//What If We Return Multiple Statements
+
+// function sub() {
+//   return 1, 2, 3, "Hello"; //iT Will Return Last Value
+// }
+// console.log(sub());
+
+// Argument Object This Is An Array Like Object Which Store Unlimmited No Of Values And It Is Given Javascript Itself
+// function Addition() {
+//   console.log(arguments);
+// }
+
+// Addition(1, 2, 3);
